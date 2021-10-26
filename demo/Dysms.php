752ba9aaecd2d83c53745e5c44272930346acad6
coffee <164589895@qq.com>
2021-10-26 08:34:30 +0800

    1

--- demo/Dysms.php
+++ demo/Dysms.php
@@ -3,3 +3,39 @@
 require "../vendor/autoload.php";
 
 
+class AliDySms
+{
+
+    private $app = null;
+
+
+    public function __construct()
+    {
+        $app                  = new \Phpfunction\Aliyun\Dysms();
+        $app->signature       = "衡水***";
+        $app->AccessKeyId     = "LTAI5***";
+        $app->AccessKeySecret = "I8dYLq***";
+
+        $this->app = $app;
+    }
+
+
+    /**
+     * 汽车保养提醒
+     */
+    public function send($tel, $tpl_code, $msg)
+    {
+        return $this->app->send($tel, $tpl_code, $msg);
+    }
+
+
+}
+
+
+$code = rand(1000, 9999);
+
+return (new AliDySms())->send('13520***', 'SMS_226***', ['code' => $code]);
+
+
+
+
